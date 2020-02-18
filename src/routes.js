d2a2f0a8337c9b87e6fc2fef4060e95a32658c49
Leandro Martins <leandromartins85@gmail.com>
2020-02-18 18:42:02 -0300

    Middleware de Autenticação

--- src/routes.js
+++ src/routes.js
@@ -2,10 +2,15 @@ import { Router } from 'express';
 import SessionController from './app/controllers/SessionController';
 import RecipientsController from './app/controllers/RecipientsController';
 
+import authMiddleware from './app/middlewares/auth';
+
 const routes = new Router();
 
 routes.post('/sessions', SessionController.store);
 
+routes.use(authMiddleware);
+
+// Rotas que precisam de autenticação
 routes.post('/recipients', RecipientsController.store);
 
 export default routes;
